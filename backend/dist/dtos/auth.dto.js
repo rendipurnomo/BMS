9ff6f409b8331470377e3f89dde6cc3769918334
rendipu <Rendipur234@gmail.com>
2026-06-21 23:30:20 +0700

    Please provide the code changes or file diffs you would like me to summarize.

--- backend/dist/dtos/auth.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LoginRequestSchema = void 0;
-const zod_1 = require("zod");
-exports.LoginRequestSchema = zod_1.z.object({
-    nrp: zod_1.z.string({ message: 'NRP is required' }).trim().min(1, 'NRP cannot be empty'),
-    password: zod_1.z.string({ message: 'Password is required' }).min(1, 'Password cannot be empty'),
-});
--- backend/dist/src/dtos/photo.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UploadPhotoSchema = void 0;
-const zod_1 = require("zod");
-exports.UploadPhotoSchema = zod_1.z.object({
-    photoType: zod_1.z.string({ message: 'Photo Type is required' }).trim().min(1, 'Photo Type cannot be empty'),
-    photoUrl: zod_1.z.string({ message: 'Photo URL is required' }).min(1, 'Photo URL cannot be empty'),
-});
